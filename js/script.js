53885c17dfd9862eef07509ecf75e0c121cda0b7
originals-yosukesato <jsollent@gmail.com>
2025-05-23 11:47:07 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
-//GSAP + ScrollTrigger
-gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-const historyContainer = document.querySelector(".history-container");
+  const historyContainer = document.querySelector(".history-container");
+  if (!historyContainer) return;
 
-gsap.to(historyContainer, {
-  x: () => -(historyContainer.scrollWidth - window.innerWidth),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".c-s-history",
-    start: "top top",
-    end: () => "+=" + (historyContainer.scrollWidth * 1.2),
-    scrub: true,
-    pin: true,
-    anticipatePin: 1,
-  },
+  const scrollLength = historyContainer.scrollWidth - window.innerWidth;
+
+  gsap.to(historyContainer, {
+    x: -scrollLength,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".c-s-history",
+      start: "top top",
+      end: () => "+=" + (historyContainer.scrollWidth - window.innerWidth + 500),
+      scrub: true,
+      pin: ".history-container",
+      anticipatePin: 1,
+      invalidateOnRefresh: true,
+    },
+  });
+
+  gsap.utils.toArray(".company-info li").forEach((item) => {
+    gsap.from(item, {
+      opacity: 0,
+      y: 50,
+      duration: 0.6,
+      scrollTrigger: {
+        trigger: item,
+        start: "top 80%",
+        toggleActions: "play none none none",
+      },
+    });
+  });
 });
